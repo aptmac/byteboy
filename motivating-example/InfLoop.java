8d61166c2e2ed27931d9fdfc86320b40b60f9dbd
aptmac <apmacdon@sfu.ca>
2017-10-24 12:47:32 -0700

    feat(example): added a motivating example for using Byteman

---
+++ motivating-example/InfLoop.java
@@ -0,0 +1,16 @@
+public class InfLoop {
+
+    private void doWork(int i) {
+        while(i == 0) {
+            work();
+        }
+    }
+
+    private static void work() {}
+
+    public static void main(String[] args) { 
+        InfLoop inf  = new InfLoop();
+	inf.doWork(0);
+    }
+
+}
