aaf66b33f8ecb6ed8e3c9b0aa3040f2773381bcb
aptmac <apmacdon@sfu.ca>
2017-11-13 02:09:33 -0800

    feat(main.py): added basic functionality to read hprof results

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+#!/usr/bin/python3
+
+# main.py
+# CMPT 479 - Final Project
+# Contributors: itpun, aptmac
+
+import os
 import sys
 
-def main():
+def main(java_file):
+    
+    # Runs the profiler on the provided java class
+    os.system("java -agentlib:hprof=cpu=times " + java_file)
+    
+    # Reads in the results, dumps some of it to the console
+    results = open("./java.hprof.txt", "r").read()
+    print(results[results.find("CPU TIME (ms)"):])
+
     # print command line arguments
-    print "testing"
+    print("testing")
     for arg in sys.argv[1:]:
-        print arg
+        print(arg)
 
 if __name__ == "__main__":
-    main()
+    try:
+        java_file = sys.argv[1]
+        main(java_file)
+    except IndexError:
+        print("Invalid number of arguments supplied")
